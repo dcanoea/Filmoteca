0de0889a0d645e8ddd4cd9c5c37da1ff3094633b
David Cano <dcanoea@fpvirtualaragon.es>
2025-01-15 17:10:17 +0100

    fin ejercicio 13

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
@@ -1,11 +1,17 @@
 package com.campusdigitalfp.filmoteca.screens
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.ImageView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,9 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.R
@@ -29,6 +39,7 @@ import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
 @Composable
 fun FilmDataScreen(navController: NavHostController, nombrePelicula : String) {
     var resultadoEditado by remember { mutableStateOf<String?>(null) }
+    val context = LocalContext.current
 
     FilmotecaTheme {
         Scaffold(topBar = {
@@ -37,41 +48,69 @@ fun FilmDataScreen(navController: NavHostController, nombrePelicula : String) {
                 atras = true
             )
         }) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Row {
-                    Text(text = "Película: $nombrePelicula")
+                verticalArrangement = Arrangement.Top) {
+
+                Row (modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 120.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.cartel),
+                        contentDescription = stringResource(R.string.imagen_de_perfil),
+                        modifier = Modifier.size(150.dp)
+                    )
+
+                    Spacer(modifier = Modifier.padding(20.dp))
+
+                    Column {
+                        Text(text = "Nombre: $nombrePelicula")
+                        Text(stringResource(R.string.director))
+                        Text(stringResource(R.string.nombredirector))
+                        Text(stringResource(R.string.a_o))
+                        Text(stringResource(R.string.numa_o))
+                        Text(stringResource(R.string.genero))
+                        Text(stringResource(R.string.formato))
+                    }
                 }
+
+
+
+
 
                 // Mostrar resultado si la película fue editada o no
                 resultadoEditado?.let {
                     Text(text = it, style = MaterialTheme.typography.bodyLarge)
                 }
 
+// fila ------------------------------------------------------------------------------
                 Row {
-                    Button(onClick = { navController.navigate("FilmDataScreen/Película relacionada") }) {
-                        Text(stringResource(R.string.ver_pel_cula_relacionada))
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = {
+                        abrirPaginaWeb("https://www.imdb.com/es-es/", context)
+                    }) {
+                        Text(stringResource(R.string.ver_en_imdb))
                     }
                 }
-
-                Row {
-                    Button(onClick = { navController.navigate("FilmEditScreen") }) {
-                        Text(stringResource(R.string.editar_pel_cula))
-                    }
-                }
-
-                Row {
-                    Button(onClick = {
+// fila ------------------------------------------------------------------------------
+                Row (modifier = Modifier.fillMaxWidth()){
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), onClick = {
                         navController.popBackStack(route = "FilmListScreen", inclusive = false)
                     }) {
-                        Text(text = stringResource(R.string.volver_a_la_principal))
+                        Text(stringResource(R.string.volver))
+                    }
+                    Spacer(modifier= Modifier.padding(all = 4.dp))
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), onClick = { navController.navigate("FilmEditScreen") }) {
+                        Text(stringResource(R.string.editar))
                     }
                 }
+
+
             }
         }
         // Lógica para manejar el resultado de FilmEditScreen
@@ -81,5 +120,17 @@ fun FilmDataScreen(navController: NavHostController, nombrePelicula : String) {
                     resultadoEditado = it
                 }
         }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun FilmDataScreenPreview() {
+    FilmotecaTheme {
+        FilmDataScreen(
+            navController = NavHostController(LocalContext.current),
+            nombrePelicula = "Pelicula"
+        )
     }
 }
