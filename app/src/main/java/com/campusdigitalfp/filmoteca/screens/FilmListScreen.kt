d37709ec1067be7d9621627240b3e4cc949499db
David Cano <dcanoea@fpvirtualaragon.es>
2025-01-16 01:55:23 +0100

    fin ejercicio 16

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
@@ -1,16 +1,35 @@
 package com.campusdigitalfp.filmoteca.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.campusdigitalfp.filmoteca.R
 import com.campusdigitalfp.filmoteca.common.BarraSuperiorComun
 import com.campusdigitalfp.filmoteca.common.Film
 import com.campusdigitalfp.filmoteca.common.FilmDataSource
@@ -57,11 +76,41 @@ fun FilmListScreen(navController: NavHostController) {
 
 @Composable
 fun FilmList(films: List<Film>) {
-    LazyColumn (modifier = Modifier.padding(horizontal = 20.dp)) {
+    LazyColumn {
         items(films) { film ->
+            FilmItem(film)
+        }
+    }
+}
+
+@Composable
+fun FilmItem(film: Film) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = film.imageResId),
+            contentDescription = film.title,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colorScheme.surface)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
             Text(
                 text = film.title.toString(),
-                maxLines = 1
+                style = MaterialTheme.typography.titleMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Director: ${film.director}",
+                style = MaterialTheme.typography.bodySmall
             )
         }
     }
